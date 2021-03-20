8fd80d0a670ea22622808e8e1a64edbc6ba5549b
Eric Londaits <eric.londaits@gmail.com>
2021-03-20 10:54:58 -0300

    Add boilerplate code with build environment using webpack.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # future-mobility
+
 An exhibit about the Future of Mobility
+
+## License
+
+Copyright (c) 2021 IMAGINARY gGmbH
+Licensed under the MIT license (see LICENSE)
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'assets'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      // Load a custom template (lodash by default)
+      template: path.resolve(__dirname, 'src/html/index.html'),
+      filename: path.resolve(__dirname, "index.html"),
+      minify: true,
+    }),
+    new CleanWebpackPlugin({
+      // todo: temporary measure. Dev builds should be done without hashes in the filename.
+      cleanAfterEveryBuildPatterns: ['**/*'],
+    }),
+  ],
+  mode: 'development',
+  // Todo: change the source map settings for production builds
+  devtool: 'source-map',
+};
