8ee59eac6c89deacb94597dee3d5e4075b93f8a7
jbalzer12 <61976072+jbalzer12@users.noreply.github.com>
2023-06-07 13:47:14 +0200

    separated into small and big turbines

--- src/js/main-devtool.js
+++ src/js/main-devtool.js
@@ -94,7 +94,8 @@ cfgLoader
     textureLoader.addSpritesheet("roads-walkable");
     textureLoader.addSpritesheet("parks");
     textureLoader.addSpritesheet("water");
-    textureLoader.addSpritesheet("windturbines"); // CHANGE HERE (ADD)
+    textureLoader.addSpritesheet("windturbines-small"); // CHANGE HERE (ADD)
+    textureLoader.addSpritesheet("windturbines-big"); // CHANGE HERE (ADD)
     textureLoader.addFolder("cars", CarSpawner.allTextureIds(config));
     textureLoader
       .load()
