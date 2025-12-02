2a7cf93b170e34d8c8267ef75f420feb185b5eb5
Eric Londaits <eric.londaits@gmail.com>
2025-12-02 21:12:49 +0100

    feat: Support titles in Dashboard action pane.

--- src/js/lib/dashboard/dashboard-app.js
+++ src/js/lib/dashboard/dashboard-app.js
@@ -79,6 +79,11 @@ class DashboardApp {
             $('<div></div>')
               .addClass(['row', 'h-18'])
               .append(
+                config?.dashboard?.actions?.title
+                  ? createTitle(config?.dashboard?.actions?.title)
+                  : null
+              )
+              .append(
                 $('<div></div>')
                   .attr('id', 'col-actions')
                   .addClass('col-8')
